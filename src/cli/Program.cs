f829b4d071fd610d73f1133167e5d2e7813bb1da
Nightowl286 <nightowl286@protonmail.com>
2026-06-06 07:24:48 +0100

    Create a basic CLI entry point.

--- src/cli/Program.cs
+++ src/cli/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Reflection;
+
+CommandApp app = new();
+
+app.Configure(config =>
+{
+	config
+		.SetApplicationName("owl")
+		.SetApplicationVersion(GetVersion());
+});
+
+return await app.RunAsync(args);
+
+static string GetVersion()
+{
+	AssemblyInformationalVersionAttribute? attribute = Assembly
+		.GetExecutingAssembly()
+		.GetCustomAttribute<AssemblyInformationalVersionAttribute>();
+
+	if (attribute is null)
+		return "dev";
+
+	string version = attribute.InformationalVersion;
+	int index = version.IndexOf('+');
+
+	if (index < 0 || index + 1 == version.Length)
+		return $"dev";
+
+	const int shortLength = 7;
+	string commitHash = version[(index + 1)..];
+	string shortHash = commitHash.Length < shortLength ? commitHash : commitHash[..shortLength];
+
+	return $"dev-{shortHash}";
+}
