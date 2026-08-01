aef2dac5e1ea0f5ecfbe2ef55687b2d1d0237884
Nightowl286 <nightowl286@protonmail.com>
2026-08-01 22:13:23 +0100

    Add empty configuration project.

---
+++ src/config/Config/global/global.Generated.cs
@@ -0,0 +1,10 @@
+global using System.CodeDom.Compiler;
+
+global using OwlDomain.ParsingTools;
+global using OwlDomain.ParsingTools.Syntax;
+global using OwlDomain.ParsingTools.Syntax.Kinds;
+global using OwlDomain.ParsingTools.Sources;
+global using OwlDomain.ParsingTools.Trivia;
+global using OwlDomain.ParsingTools.Positioning;
+global using OwlDomain.ParsingTools.Positioning.Ranges;
+global using OwlDomain.ParsingTools.Classification;
