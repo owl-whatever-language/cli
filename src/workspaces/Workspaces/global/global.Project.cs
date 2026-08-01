ec448fb9fbd3f32a8e35eab5190a06d22d7b10ca
Nightowl286 <nightowl286@protonmail.com>
2026-08-01 22:39:19 +0100

    Remove the workspace and package languages.

--- src/workspaces/Workspaces/global/global.Project.cs
+++ src/workspaces/Workspaces/global/global.Project.cs
@@ -1,9 +1,4 @@
 global using CommunityToolkit.Diagnostics;
 
-global using OwlDomain.ParsingTools.Diagnostics;
-global using OwlDomain.ParsingTools.Results;
-
-global using OwlDomain.ParsingTools.Parsing;
-global using OwlDomain.ParsingTools.Text;
-
-global using OwlDomain.Owl.Workspaces.CodeAnalysis.Parsing;
+global using OwlDomain.ParsingTools;
+global using OwlDomain.ParsingTools.Sources;
